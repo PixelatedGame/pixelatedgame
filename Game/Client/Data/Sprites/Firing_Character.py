64d08368887f2f6875a8918b41eaad9d4608ddb2
timmy <timmy@10.0.0.3>
2013-10-08 23:23:55 +0300

    Firing

---
+++ Game/Client/Data/Sprites/Firing_Character.py
@@ -0,0 +1,16 @@
+from Game.Client.Data.Sprites.Character import Character
+from Game.Client.Data.Sprites.fire import Fire
+
+
+
+class Firing_Character(Character):
+    def __init__(self, internal_name, char_name):
+        Character.__init__(self, internal_name, char_name)
+        self.isFiring = False
+        self.ranged = Fire("fireball",self)
+        #        fireball.fire_fireball(Game.Client.my_char.data.rect.copy(), Game.Client.my_char.data.right)
+    
+    def _fire(self):
+        if not self.isFiring:
+            self.isFiring = True   
+            self.ranged.fire_fireball()
